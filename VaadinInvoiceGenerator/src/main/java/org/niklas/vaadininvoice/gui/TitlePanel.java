7669d66f3b40dcf23eefeb63390146f5f0dd4406
unknown <nyholmniklas@gmail.com>
2014-03-10 00:58:47 +0200

    Made download link a button and corrected alignement of title panel buttons

--- VaadinInvoiceGenerator/src/main/java/org/niklas/vaadininvoice/gui/TitlePanel.java
+++ VaadinInvoiceGenerator/src/main/java/org/niklas/vaadininvoice/gui/TitlePanel.java
@@ -2,21 +2,26 @@ package org.niklas.vaadininvoice.gui;
 
 import java.io.File;
 
+import com.vaadin.server.FileDownloader;
+import com.vaadin.server.FileResource;
 import com.vaadin.server.ThemeResource;
 import com.vaadin.shared.ui.label.ContentMode;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Layout;
 import com.vaadin.ui.Link;
 import com.vaadin.ui.Panel;
 
 public class TitlePanel extends Panel{
-	private HorizontalLayout layout;
+	private GridLayout layout;
 	private Label titleLabel;
 	private Button createButton;
 	
 	public TitlePanel(Button button){
-		layout = new HorizontalLayout();
+		layout = new GridLayout(3,1);
 		titleLabel = new Label("<h1>Create New Invoice</h1>", ContentMode.HTML);
 		titleLabel.setWidth(300, Unit.PIXELS);;
 		this.createButton = button;
@@ -24,15 +29,22 @@ public class TitlePanel extends Panel{
 	}
 	
 	private void setLayout() {
-		layout.addComponent(titleLabel);
-		layout.addComponent(createButton);
+		layout.addComponent(titleLabel, 0, 0);
+		layout.setComponentAlignment(titleLabel, Alignment.MIDDLE_LEFT);
+		layout.addComponent(createButton, 1, 0);
+		layout.setComponentAlignment(createButton, Alignment.MIDDLE_RIGHT);
+		layout.setSizeFull();
 		layout.setSpacing(true);
 		layout.setMargin(true);
 		setContent(layout);
 	}
 
 	protected void setLink(File file) {
-		Link link = new DownloadLink(file);
-		layout.addComponent(link);
+		layout.removeComponent(2, 0);
+		Button downloadButton = new Button("View PDF");
+		FileDownloader fileDownloader = new FileDownloader(new FileResource(file));
+		fileDownloader.extend(downloadButton);
+		layout.addComponent(downloadButton, 2,0);
+		layout.setComponentAlignment(downloadButton, Alignment.MIDDLE_LEFT);
 	}
 }
