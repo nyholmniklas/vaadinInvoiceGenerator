f33f6e0e3c361a283651e903ea44169b30253ce7
Niklas <nyholmniklas@gmail.com>
2014-03-14 22:05:54 +0200

    Logo draws properly on pdf. Rearranged pdf impl.

--- VaadinInvoiceGenerator/src/main/java/org/niklas/vaadininvoice/invoice2pdf/Invoice2PdfBoxImpl.java
+++ VaadinInvoiceGenerator/src/main/java/org/niklas/vaadininvoice/invoice2pdf/Invoice2PdfBoxImpl.java
@@ -18,12 +18,14 @@ import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.pdmodel.graphics.xobject.PDJpeg;
+import org.niklas.vaadininvoice.model.Address;
 import org.niklas.vaadininvoice.model.Invoice;
 import org.niklas.vaadininvoice.model.InvoiceRow;
 
 public class Invoice2PdfBoxImpl implements Invoice2Pdf{
 	private final PDType1Font normalFont = PDType1Font.HELVETICA;
 	private final PDType1Font boldFont = PDType1Font.HELVETICA_BOLD;
+	private final PDType1Font italicFont = PDType1Font.TIMES_ITALIC;
 
 	public File getPdfFromInvoice(Invoice invoice, String sessionId) {
 		PDDocument doc = null;
@@ -44,7 +46,10 @@ public class Invoice2PdfBoxImpl implements Invoice2Pdf{
 	
 	private PDPage createPage(PDDocument doc, Invoice invoice) throws IOException {
 		PDPage page = new PDPage();
-		
+		float mediaBoxWidth = page.getMediaBox().getWidth();
+		float mediaBoxHeight = page.getMediaBox().getHeight();
+		System.out.println("MediaBox dimensions are " + mediaBoxWidth + "x"
+				+ mediaBoxHeight);
 		//Draw logo here because of contentstream and imager creation order bug!! Look into generalized method of drawing images on pdf
 		PDPageContentStream contentStream;
 		if (invoice.getLogoImageFilePath() != null) {
@@ -56,51 +61,47 @@ public class Invoice2PdfBoxImpl implements Invoice2Pdf{
 //			img.setWidth((int) Math.round(logoDimensions.getWidth()));
 //			img.setHeight((int) Math.round(logoDimensions.getHeight()));
 //			contentStream.drawImage(img, 50, 50);
-			contentStream.drawXObject(img, 50f, 50f, (float)logoDimensions.getWidth(), (float) logoDimensions.getHeight());
+			contentStream.drawXObject(img, 50f, 692f, (float)logoDimensions.getWidth(), (float) logoDimensions.getHeight());
 		}
 		else {
 			contentStream = new PDPageContentStream(doc, page);
 		}
 
-		
-		writeCustomerAddress(invoice, page, contentStream);
-		writeCompanyAddress(invoice, page, contentStream);
-		writeInvoiceInfo(invoice, page, contentStream);
-		writeInvoiceRows(invoice, page, contentStream);
-		writeDescription(invoice, page, contentStream);
+		writeBoldText(175, 662, "BILL TO: ", contentStream);
+		writeAddress(175, 642, invoice.getCustomer(), page, contentStream);
+//		writeItalicText(50, 662, "From: ", contentStream);
+		writeAddressItalic(50, 642, invoice.getCompany(), page, contentStream);
+		writeInvoiceInfo(350, 662, invoice, page, contentStream);
+		writeInvoiceRows(50, 450, invoice, page, contentStream);
+		writeDescription(50, 525, invoice, page, contentStream);
 		contentStream.close();
 		return page;
 	}
 	
-	private PDPage writeCustomerAddress(Invoice invoice, PDPage page, PDPageContentStream contentStream) throws IOException {
-		writeBoldText(100, 720, "Billable", contentStream);
-		writeText(100, 700, invoice.getCustomer().getName(), contentStream);
-		writeText(100, 685, invoice.getCustomer().getAddress(), contentStream);
-		writeText(100, 670, invoice.getCustomer().getPostcode(), contentStream);
-		writeText(100, 655, invoice.getCustomer().getCity(), contentStream);
+	
+	
+	private PDPage writeAddress(int x, int y, Address address, PDPage page, PDPageContentStream contentStream) throws IOException {
+		writeText(x, y, address.getName(), contentStream);
+		writeText(x, y -= 17, address.getAddress(), contentStream);
+		writeText(x, y -= 17, address.getPostcode(), contentStream);
+		writeText(x, y -= 17, address.getCity(), contentStream);
 		return page;
 	}
 	
-	private PDPage writeCompanyAddress(Invoice invoice, PDPage page, PDPageContentStream contentStream) throws IOException {
-		writeBoldText(250, 720, "Payable to", contentStream);
-		writeText(250, 700, invoice.getCompany().getName(), contentStream);
-		writeText(250, 685, invoice.getCompany().getAddress(), contentStream);
-		writeText(250, 670, invoice.getCompany().getPostcode(), contentStream);
-		writeText(250, 655, invoice.getCompany().getCity(), contentStream);
+	private PDPage writeAddressItalic(int x, int y, Address address, PDPage page, PDPageContentStream contentStream) throws IOException {
+		writeItalicText(x, y, address.getName(), contentStream);
+		writeItalicText(x, y -= 17, address.getAddress(), contentStream);
+		writeItalicText(x, y -= 17, address.getPostcode(), contentStream);
+		writeItalicText(x, y -= 17, address.getCity(), contentStream);
 		return page;
 	}
 	
-//	private PDPage drawLogo(Invoice invoice, PDPage page, PDDocument doc) throws IOException {
-//		InputStream in = new FileInputStream(new File(invoice.getLogoImageFilePath()));
-//		PDJpeg img = new PDJpeg(doc, in);
-//		//Have to create new content stream to avoid bug with pdfbox
-//		PDPageContentStream contentStream = new PDPageContentStream(doc, page);
-//		img.setWidth(200);
-//		contentStream.drawImage(img, 100, 700);
-//		contentStream.close();
-//		return page;
-//	}
-	
+	private void writeItalicText(int x, int y, String value,
+			PDPageContentStream contentStream) throws IOException {
+		writeTextWithFont(x, y, value, contentStream, italicFont);
+		
+	}
+
 	private void writeText(int x, int y, String value, PDPageContentStream contentStream) throws IOException {		
 		writeTextWithFont(x, y, value, contentStream, normalFont);
 	}
@@ -118,49 +119,52 @@ public class Invoice2PdfBoxImpl implements Invoice2Pdf{
 		contentStream.endText();
 	}
 	
-	private PDPage writeInvoiceInfo(Invoice invoice, PDPage page, PDPageContentStream contentStream) throws IOException {
+	private PDPage writeInvoiceInfo(int x, int y, Invoice invoice, PDPage page, PDPageContentStream contentStream) throws IOException {
 		DateFormat df = new SimpleDateFormat("dd.MM.YYYY");
-		writeBoldText(370, 720, "Invoice Date", contentStream);
-		writeText(370, 700, df.format(invoice.getInvoiceDate()), contentStream);
-		writeBoldText(490, 720, "Due Date", contentStream);
-		writeText(490, 700, df.format(invoice.getDueDate()), contentStream);
-		writeBoldText(370, 680, "Invoice Number", contentStream);
-		writeText(370, 665, ""+invoice.getInvoiceNumber(), contentStream);
-		writeBoldText(490, 680, "Reference", contentStream);
-		writeText(490, 665, ""+ invoice.getReferenceNumber(), contentStream);
+		writeBoldText(x, y, "Invoice Date", contentStream);
+		writeText(x, y - 14, df.format(invoice.getInvoiceDate()), contentStream);
+		writeBoldText(x, y - 50, "Due Date", contentStream);
+		writeText(x, y - 64, df.format(invoice.getDueDate()), contentStream);
+		writeBoldText(x + 100, y, "Invoice Number", contentStream);
+		writeText(x+100, y - 14, ""+invoice.getInvoiceNumber(), contentStream);
+		writeBoldText(x+100, y -50, "Reference", contentStream);
+		writeText(x+100, y - 64, ""+ invoice.getReferenceNumber(), contentStream);
 		return page;
 	}
 	
-	private PDPage writeInvoiceRows(Invoice invoice, PDPage page, PDPageContentStream contentStream) throws IOException {
-		writeBoldText(100, 500, "Quantity", contentStream);
-		writeBoldText(200, 500, "Description", contentStream);
-		writeBoldText(300, 500, "Price", contentStream);
-		writeBoldText(400, 500, "Tax Rate %", contentStream);
-		writeBoldText(500, 500, "Total", contentStream);
+	private PDPage writeInvoiceRows(int x, int y, Invoice invoice, PDPage page, PDPageContentStream contentStream) throws IOException {
+		writeBoldText(x, y, "Quantity", contentStream);
+		writeBoldText(x+100, y, "Description", contentStream);
+		writeBoldText(x+200, y, "Price", contentStream);
+		writeBoldText(x+300, y, "Tax Rate %", contentStream);
+		writeBoldText(x+400, y, "Total", contentStream);
 		
 		int i = 0;
 		for (; i < invoice.getRows().size() ; i++) {
 			InvoiceRow row = invoice.getRows().get(i);
-			int y = 460 - (14*i);
-			writeText(100, y, row.getDescription(), contentStream);
-			writeText(200, y, Integer.toString(row.getQuantity()), contentStream);
-			writeText(300, y, row.getPrice().toString(), contentStream);
-			writeText(400, y, row.getTaxRate().toString(), contentStream);
-			writeText(500, y, row.getTotalToString(), contentStream);
+			y = y - 25 - (14*i);
+			writeText(x, y, row.getDescription(), contentStream);
+			writeText(x+100, y, Integer.toString(row.getQuantity()), contentStream);
+			writeText(x+200, y, row.getPrice().toString(), contentStream);
+			writeText(x+300, y, row.getTaxRate().toString(), contentStream);
+			writeText(x+400, y, row.getTotalToString(), contentStream);
 		}
-		
-		writeBoldText(400, 440 - (14*i), "TOTAL: ", contentStream);
-		writeBoldText(500, 440 - (14*i), invoice.getTotal().toString(), contentStream);
+		writeText(x+300, y - 50 - (14*i), "Sub-Total: ", contentStream);
+		writeText(x+400, y - 50 - (14*i), invoice.getSubTotal().toString()+" EUR", contentStream);
+		writeText(x+300, y - 50 - (14*i--), "VAT Total: ", contentStream);
+		writeText(x+400, y - 50 - (14*i), invoice.getVatTotal().toString()+" EUR", contentStream);
+		writeBoldText(x+300, y - 50 - (14*i--), "TOTAL: ", contentStream);
+		writeBoldText(x+400, y - 50 - (14*i), invoice.getTotal().toString()+" EUR", contentStream);
 		
 		return page;
 	}
 	
 	
-	private PDPage writeDescription(Invoice invoice, PDPage page, PDPageContentStream contentStream) throws IOException {
+	private PDPage writeDescription(int x, int y, Invoice invoice, PDPage page, PDPageContentStream contentStream) throws IOException {
 	    float fontSize = 12;
 	    float leading = 1.5f * fontSize;
-	    int marginX = 100;
-	    int marginY = 600;
+	    int marginX = x;
+	    int marginY = y;
 	    
 	    PDRectangle mediabox = page.findMediaBox();
 	    float width = mediabox.getWidth() - 2*marginX;
@@ -212,16 +216,16 @@ public class Invoice2PdfBoxImpl implements Invoice2Pdf{
 	}
 	
 	private Dimension getImageDimensions(PDJpeg img) {
-		float maxWidth = 100;
-		float maxHeight = 50;
+		float maxWidth = 150;
+		float maxHeight = 100;
 		float width = img.getWidth();
 		float height = img.getHeight();
 		System.out.println("Image of size "+width+"x"+height+"px was resized to:");
-		if (width > maxWidth) {
+		while (width > maxWidth) {
 			height = (maxWidth / width) * height;
 			width = maxWidth;
 		}
-		if (height > maxHeight) {
+		while (height > maxHeight) {
 			width = (maxHeight / height) * width;
 			height = maxHeight;
 		}
